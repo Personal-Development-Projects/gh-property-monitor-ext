3d4ab15b87b2cc835105a7afc0f1bdeabc47c947
Patrick MacbookPro <p.oconnormsu@gmail.com>
2023-01-28 14:44:27 -0700

    Still need to get output mapping to go object and excel?
    Also need to get a better way to handle errors

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/cli/go-gh"
+	"log"
 )
 
 func main() {
 
 	// Using github rest api to get username that is logged in at enterprise server
 	client, err := gh.RESTClient(nil)
-	if err != nil {
-		fmt.Println(err)
+	if funcName(err) {
 		return
 	}
 	response := struct{ Login string }{}
@@ -23,90 +24,29 @@ func main() {
 	fmt.Printf("Running as %s\n", response.Login)
 
 	fmt.Println("Collecting PRs associated with branch")
-	/*	updatedPRBuffer, _, err := gh.Exec("search", "prs", "--repo", "Personal-Development-Projects/OConnor-Development-Project.github.io", "--json", "number", "--jq", ".[].number")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(updatedPRBuffer)
-
-		prListResult := &Results{}
-		err = json.Unmarshal(updatedPRBuffer.Bytes(), prListResult)
-		if err != nil {
-			fmt.Println(err)
-		}*/
-
-	tempTest, _, err := gh.Exec("api", "graphql", "-F", "$org", "Personal-Development-Projects", "-F", "$repo", "OConnor-Development-Project.github.io", "-F", "query", "query allPullRequests($org: String!, $repo: String!, $endCursor: String) { \n  organization(login: $org) { \n    repository(name: $repo) { \n      pullRequests(first: 100, after: $endCursor, states: OPEN,) { \n nodes { \n author { \n login \n } \n number \n createdAt \n mergedAt \n mergedBy { \n login \n } \n approvers: reviews(states: APPROVED, first: 10) { \n nodes { \n author { \n ... on User { \n name \n login \n } \n } \n state \n } \n } \n title \n repository { \n owner { \n login \n } \n name \n } \n } \n pageInfo { \n hasNextPage \n endCursor \n } \n } \n } \n } \n} ")
+	updatedPRBuffer, _, err := gh.Exec("search", "prs", "--repo", "Personal-Development-Projects/OConnor-Development-Project.github.io", "--json", "number", "--jq", ".[].number")
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
-	fmt.Println(tempTest.String())
+	fmt.Println("Starting to collect the file changes from PR's")
+	scanner := bufio.NewScanner(&updatedPRBuffer)
+	for scanner.Scan() {
+		getPRDetails(scanner.Text())
+		//fmt.Printf("metric: %s", scanner.Text())
+	}
 
-	//err = json.Unmarshal(updatedPR.Bytes(), &prListResult)
-	//if err != nil {
-	//	fmt.Println(err)
-	//	fmt.Println(updatedPR.Bytes())
-	//	return
-	//}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-	//if err := json.Unmarshal(updatedPR.Bytes(), &prListResult); err != nil {
-	//	log.Fatal(err)
-	//}
-	// gh api \
-	//  -H "Accept: application/vnd.github+json" \
-	//  /repos/OWNER/REPO/pulls/PULL_NUMBER
-	// USE FOR Getting PR number with
-	//updatedP, _, err := gh.Exec("api", "Accept", "application/vnd.github.diff+json", "--repo", "Personal-Development-Projects/OConnor-Development-Project.github.io/pulls/1")
-
-	//var r Result
-	//if err := json.Unmarshal(updatedPR.Bytes(), &r); err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Println(r.Branch)
-	//splitString := strings.Split(updatedPR.String(), ",")
-	//for prNumber := range splitString {
-	//	getPRDetails(prNumber)
-	//}
 }
 
-//func getPRDetails(prNumber int) {
-//	gh.Exec("pr", "--repo", "Personal-Development-Projects/OConnor-Development-Project.github.io", "diff", string(prNumber))
-//}
-
-//func (result *PRListResult) UnmarshalJSON(p []byte) error {
-//	var tmp []json.RawMessage
-//	if err := json.Unmarshal(p, &tmp); err != nil {
-//		return err
-//	}
-//	if err := json.Unmarshal(tmp[0], &result.Number); err != nil {
-//		return err
-//	}
-//
-//	if len(tmp) > 1 {
-//		if err := json.Unmarshal(tmp[1], &result.Branch); err != nil {
-//			return err
-//		}
-//		if err := json.Unmarshal(tmp[2], &result.FilesChanged); err != nil {
-//			return err
-//		}
-//		if err := json.Unmarshal(tmp[3], &result.Changes); err != nil {
-//			return err
-//		}
-//	}
-//	return nil
-//}
-
-type Results struct {
-	_ []PullRequest
+func getPRDetails(prNumber string) {
+	prDetails, _, err := gh.Exec("pr", "--repo", "Personal-Development-Projects/OConnor-Development-Project.github.io", "diff", prNumber)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Print(prDetails.String())
+	//fmt.Println(_prDetails)
 }
-
-// Result base object for PRs
-
-type PullRequest struct {
-	number string
-}
-
-//const (
-//	PrListQuery string = "' \n query allPullRequests($org: String!, $repo: String!, $endCursor: String) { \n  organization(login: $org) { \n    repository(name: $repo) { \n      pullRequests(first: 100, after: $endCursor, states: OPEN,) { \n nodes { \n author { \n login \n } \n number \n createdAt \n mergedAt \n mergedBy { \n login \n } \n approvers: reviews(states: APPROVED, first: 10) { \n nodes { \n author { \n ... on User { \n name \n login \n } \n } \n state \n } \n } \n title \n repository { \n owner { \n login \n } \n name \n } \n } \n pageInfo { \n hasNextPage \n endCursor \n } \n } \n } \n } \n} \n '"
-//	PrNumQuery  string = "summer"
-//)
