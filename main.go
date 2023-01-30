2c174098c7c8974566373b7592a1dc281b1fabf8
Tyler Redding <tyler.redding.code@outlook.com>
2023-01-30 11:58:47 -0700

    Added sorting logic and started func for writing data file

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"github.com/cli/go-gh"
+	"os"
 	"strconv"
 	"strings"
+
+	"github.com/cli/go-gh"
 )
 
 func main() {
@@ -45,7 +47,11 @@ func main() {
 	//		textAdditions     []string
 	//		textDeletions     []string
 	var finalResult = populateDetailedResults(prBaseResults)
-	fmt.Println(finalResult)
+	writeResultsToFile(finalResult)
+	fmt.Println(finalResult[0].DetailedResults.FileAdditions)
+	fmt.Println(finalResult[0].DetailedResults.TxtAdditions)
+	fmt.Println(finalResult[0].DetailedResults.FileDeletions)
+	fmt.Println(finalResult[0].DetailedResults.TxtDeletions)
 }
 
 // TODO This could be improved by increasing the proficiency of the parser
@@ -59,16 +65,30 @@ func populateDetailedResults(prList PullRequestsResults) PullRequestsResults {
 		}
 		// parse those into struct that can be merged into prList
 		scanner := bufio.NewScanner(&prDetails)
+		scanner.Split(bufio.ScanLines)
 		for scanner.Scan() {
-			if strings.Contains(scanner.Text(), "+") {
-				prList[prIndex].DetailedResults.Additions = append(prList[prIndex].DetailedResults.Additions, scanner.Text())
+			if strings.HasPrefix(scanner.Text(), "+") && strings.Count(scanner.Text(), "+") >= 3 {
+				prList[prIndex].DetailedResults.FileAdditions = append(prList[prIndex].DetailedResults.FileAdditions, scanner.Text())
+			} else if strings.HasPrefix(scanner.Text(), "+") && strings.Count(scanner.Text(), "+") < 3 {
+				prList[prIndex].DetailedResults.TxtAdditions = append(prList[prIndex].DetailedResults.TxtAdditions, scanner.Text())
+			} else if strings.HasPrefix(scanner.Text(), "-") && strings.Count(scanner.Text(), "-") >= 3 {
+				prList[prIndex].DetailedResults.FileDeletions = append(prList[prIndex].DetailedResults.FileDeletions, scanner.Text())
+			} else if strings.HasPrefix(scanner.Text(), "-") && strings.Count(scanner.Text(), "-") < 3 {
+				prList[prIndex].DetailedResults.TxtDeletions = append(prList[prIndex].DetailedResults.TxtDeletions, scanner.Text())
 			}
-			if strings.Contains(scanner.Text(), "-") {
-				prList[prIndex].DetailedResults.Deletions = append(prList[prIndex].DetailedResults.Deletions, scanner.Text())
-			}
+
 		}
 	}
 	return prList
+}
+
+// Need to iterate through results and write to file in correct format
+func writeResultsToFile(resultList PullRequestsResults) {
+	file, err := os.Create("data.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
 }
 
 type PullRequestsResults []struct {
@@ -85,7 +105,9 @@ type PullRequest struct {
 		//NameWithOwner string `json:"nameWithOwner"`
 	} `json:"repository"`
 	DetailedResults struct {
-		Additions []string `json:"_"`
-		Deletions []string `json:"_"`
+		FileAdditions []string `json:"_"`
+		TxtAdditions  []string `json:"_"`
+		FileDeletions []string `json:"_"`
+		TxtDeletions  []string `json:"_"`
 	}
 }
